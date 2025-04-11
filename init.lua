b4e8966472157b9a1d4ed8bcb1bdd6093f289a12
Corbyn <corbyn.altman01@gmail.com>
2025-04-11 15:35:44 -0400

    add nvimtree

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -282,21 +282,52 @@ require('lazy').setup({
     },
   },
   {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+
+      vim.keymap.set('n', '<A-e>', function()
+        require('nvim-tree.api').tree.toggle()
+      end)
+    end,
+  },
+  {
     'akinsho/toggleterm.nvim',
     version = '*',
     config = function()
       require('toggleterm').setup {
-        size = 10,
-        open_mapping = [[<c-\>]],
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-          title_pos = 'left',
-        },
+        -- Your existing options:
+        size = 60,
+        open_mapping = [[<c-\>]], -- Toggles the last used/default terminal
+        direction = 'vertical',
+        -- float_opts = {
+        --   border = 'curved',
+        --   title_pos = 'left',
+        -- },
+        -- Optional: Close the terminal buffer when the process exits
+        -- close_on_exit = true,
+        -- Optional: Persist terminal state (requires session management)
+        -- persist_size = true,
+        -- persist_mode = true, -- Note: persist_mode might interfere with switching
       }
 
+      -- Keymap for :TermSelect command (using Alt+\ as an example)
+      -- You can change <A-\> to something else if you prefer
+      vim.keymap.set({ 'n', 't' }, '<A-\\>', function()
+        -- Use the Lua function equivalent of :TermSelect
+        vim.cmd 'TermSelect'
+      end, { noremap = true, silent = true, desc = 'Select ToggleTerm' })
+
+      -- Your custom TermNew command is no longer strictly necessary
+      -- as toggling by ID (e.g., <c-\>1) will create the terminal if needed.
+      -- You can remove it or keep it if you have a specific use case for it.
       vim.api.nvim_create_user_command('TermNew', function()
-        term = require('toggleterm.terminal').Terminal:new {}
+        local term = require('toggleterm.terminal').Terminal:new {}
         term:spawn()
         term:toggle()
       end, { bang = true })
@@ -909,8 +940,9 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = false },
         },
+        transparent = true, -- Disable italics in comments
       }
 
       -- Load the colorscheme here.
